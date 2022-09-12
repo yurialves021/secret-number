fe9ac866c761683a06868fb252f8c92d8b38b299
Yuri Alves <yurialvesedf@gmail.com>
2022-09-12 14:51:27 -0300

    Capturando a voz

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -1,7 +1,24 @@
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
+const elementoChute = document.getElementById('chute')
+
 const recognition = new SpeechRecognition();
 
 recognition.lang = 'pt-Br'
 recognition.start()
 
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    const chute = e.results[0][0].transcript
+
+    exibeChute(chute)
+}
+
+function exibeChute(chute) {
+
+elementoChute.innerHTML = `
+<div>Você disse</div>
+<span class="box"> ${chute} </span>
+`
+}
